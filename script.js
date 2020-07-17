2e63f9c30bf8ca75c0a1038d1d28072fdd9dcd5d
ntongwe <ntongwe@sheridancollege.ca>
2020-07-16 21:53:48 -0400

    First commit

---
+++ script.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function(){
+
+
+addComments()
+});
+
+
+let addComments = function () {
+  // let node = document.createElement("LI");                 // Create a <li> node
+  // let textnode = document.createTextNode("Elvis");         // Create a text node
+  // node.appendChild(textnode);                              // Append the text to
+  // document.getElementById("siblings").appendChild(node); 
+}
+
+document.getElementByI()
